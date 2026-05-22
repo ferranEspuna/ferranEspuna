1fcb1074953b8065dbfef48135b1db783ef5dfe4
ferran <ferranespuna@gmail.com>
2026-05-22 21:11:28 +0200

    Extract page titles from README H1 headings and clean up unused index.md files

--- _plugins/readme_generator.rb
+++ _plugins/readme_generator.rb
@@ -41,7 +41,18 @@ module ReadmeGenerator
         full_path = File.join(site.source, rel_path)
         next unless File.exist?(full_path)
 
-        processed_paths[full_path] = meta
+        content = File.read(full_path)
+        first_non_blank_line = content.lines.map(&:strip).reject(&:empty?).first
+        extracted_title = nil
+        if first_non_blank_line && first_non_blank_line.start_with?("# ")
+          extracted_title = first_non_blank_line.sub(/^#\s+/, '').strip
+        end
+
+        processed_paths[full_path] = {
+          "permalink" => meta["permalink"],
+          "layout" => meta["layout"],
+          "title" => extracted_title || meta["title"]
+        }
       end
 
       # Walk the source directory and find all README.md files dynamically
@@ -67,14 +78,25 @@ module ReadmeGenerator
           end
         end
 
+        content = File.read(full_path)
+        first_non_blank_line = content.lines.map(&:strip).reject(&:empty?).first
+        extracted_title = nil
+        if first_non_blank_line && first_non_blank_line.start_with?("# ")
+          extracted_title = first_non_blank_line.sub(/^#\s+/, '').strip
+        end
+
         dir_name = File.dirname(rel_path)
         if dir_name == "."
           permalink = "/"
-          title = "Home"
+          title = extracted_title || "Home"
         else
           permalink = "/#{dir_name}/"
-          folder_name = File.basename(dir_name)
-          title = folder_name.split('_').map(&:capitalize).join(' ')
+          if extracted_title
+            title = extracted_title
+          else
+            folder_name = File.basename(dir_name)
+            title = folder_name.split('_').map(&:capitalize).join(' ')
+          end
         end
 
         processed_paths[full_path] = {
